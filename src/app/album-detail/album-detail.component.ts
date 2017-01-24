1ea365015e5ddda9c1867c95087297c0611c1d0b
Joshua Rinard <jrinard@hotmail.com>
2017-01-24 10:43:01 -0800

    finish service

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -2,21 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router'; //contains information about a route associated with a component loaded in an outlet.  //collect parameters for use when routing
 import { Location } from '@angular/common'; //normalize URLs when traveling between routes, especially dynamic ones.
 import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
-  styleUrls: ['./album-detail.component.css']
+  styleUrls: ['./album-detail.component.css'],
+  providers: [AlbumService]
 })
 export class AlbumDetailComponent implements OnInit {
   albumId: number = null;//created and will be filled later
+  albumToDisplay: Album;
 
-  constructor(private route: ActivatedRoute, private location: Location) { } //we're ensuring that any new AlbumDetailComponent instances are created with these objects available to them. //any instance of AlbumDetailComponent will have route and location properties that can be accessed by calling this.route and this.location.
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private albumService: AlbumService) { } //we're ensuring that any new AlbumDetailComponent instances are created with these objects available to them. //any instance of AlbumDetailComponent will have route and location properties that can be accessed by calling this.route and this.location.
 
   ngOnInit() {// is a component lifecycle hook. //Any code we place here will run when a component is initialized (that is, after constructor() creates it).
     this.route.params.forEach((urlParametersArray) => {//this.route uses ActivatedRoute, uses params to retrieve any parameters from goToDetailPage method//Loops through each temporarly assigning each paramater to urlParametersArray.
        this.albumId = parseInt(urlParametersArray['id']); //retrieves the number with the key Id. this matches the route's path, and assigns to albumId
      }); // => is a Fat arrow that basically is a short hand function. it keeps the value of "This"
+     this.albumToDisplay = this.albumService.getAlbumById(this.albumId);//gets single id gets single album via service and assigns to album to display.
    }
+
 
 }
---
+++ src/app/album.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';// pulling in mock albums
+
+@Injectable() // injector is responsible for delivering the service where it needs to be.
+export class AlbumService {
+
+  constructor() { }
+
+  getAlbums() {
+    return ALBUMS;
+  }
+
+  getAlbumById(albumId: number){
+    for (var i = 0; i <= ALBUMS.length - 1; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
+  }
+
+}
