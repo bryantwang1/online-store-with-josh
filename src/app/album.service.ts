593fa9f4d401c00c286b9b13d044732f4cfbcc8f
Bryant Wang and Joshua Rinard <bryant@>
2017-01-24 11:40:31 -0800

    Pulling data from firebase

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
 import { ALBUMS } from './mock-albums';// pulling in mock albums
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 
 @Injectable() // injector is responsible for delivering the service where it needs to be.
 export class AlbumService {
+  albums: FirebaseListObservable<any[]>;//this means we're expecting Firebase to return an array list of database entries, of any variety//FirebaseListObservable object will automatically update accordingly.
 
-  constructor() { }
+  constructor(private angularFire: AngularFire) {
+    this.albums = angularFire.database.list('albums');//call the instance of angularFire, call .database to access our database, .list to specify we're gathering a list of multiple things //albums matches database
+  }
 
   getAlbums() {
-    return ALBUMS;
+    return this.albums;
   }
 
   getAlbumById(albumId: number){
